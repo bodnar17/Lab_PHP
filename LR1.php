60c6438c2a9a81f88fbec9933503392ea09464b5
bodnar17 <56383168+bodnar17@users.noreply.github.com>
2021-10-11 13:08:07 +0300

    Add files via upload

---
+++ LR1.php
@@ -0,0 +1,14 @@
+
+<html>
+   <head>
+       <title>LR1</title>
+   </head>
+   <body>
+      <?php
+          echo "Hello, World";
+      ?>
+   </body>
+</html>
+
+
+
---
+++ LR1_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <title>LR1</title>
+   </head>
+   <body>
+      <?php
+          $name= "Vasyl";
+          echo "Hello, ".$name."!";
+      ?>
+   </body>
+</html>
+
+
